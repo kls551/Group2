642dc7b8c84b3d79830c18c9a0f6eaea5f5dc0f6
Lien (Ally) Quan <liquan@calpoly.edu>
2019-02-11 18:20:21 -0800

    addservices

--- client/src/views/Services.vue
+++ client/src/views/Services.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="services">
-    <h1>This is an services</h1>
-    <div class="columns">
-      <div class="column is-one-third is-3 is-orange"> 
-        <p> Wheels Services </p>
-        <div class="picture"> <img src="../assets/tireChange.jpg"  height="600" width="500"> </div>
-      </div>
+    <p class="title"> This is the services that FoxCycle Offer</p> 
+      <div class="tile is-ancestor"> 
+        <div class="tile is-parent is-vertical">
+          <div class="title box is-4 is-primary is-dark"> 
+            <p class="title"> Wheels Services </p>
+            <div class="picture"> <img src="../assets/tireChange.jpg"  height="200" width="200"> </div>
+          </div>
 
-      <div class="column is-one-third is-3"> 
-        <p> Brake Services </p> 
-        <div class="picture"> <img src="../assets/brakeTuning.jpeg"  height="600" width="500"> </div>
+          <div class="title box iis-vertical s-4 is-primary"> 
+            <p class="title"> Brake Services </p> 
+            <div class="picture"> <img src="../assets/brakeTuning.jpeg"  height="200" width="200"> </div>
+          </div>
+          <div class="title box is-vertical is-4 is-primary">  
+            <p class="title"> Customization </p> 
+            <div class="picture"> <img src="../assets/customBike.jpeg"  height="200" width="200"> </div>
+          </div>
+        </div>
       </div>
-    <div class="column is-one-third is-3">  
-      <p> Customization </p> 
-      <div class="picture"> <img src="../assets/customBike.jpeg"  height="500" width="500"> </div>
-    </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
+import "../assets/main.scss";
 export default Vue.extend({
   
 })
 </script>
 
-<style scoped>
+<style lang="scss">
 @import "~bulma/css/bulma.css";
 
 /* .column {

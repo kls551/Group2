9d68be2fd5bf76782b49697489b9b557ad5b6192
Kyaw Soe <32246763+kls551@users.noreply.github.com>
2019-03-10 15:54:53 -0700

    item detail multiple images

--- client/src/views/ItemView.vue
+++ client/src/views/ItemView.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="itemView">
     <div class="columns" v-if="loaded">
-      <div class="rightMargin column">
-        <figure class="image is-3by2">
-          <img alt="Map" :src="shopItem.images[0].img">
-        </figure>
-      </div>
+          <div class="card-content">
+            <div class="card-carousel">
+              <figure class="image is-48x48">
+                <img :src="currentImage" alt>
+              </figure>
+              <div class="thumbnails" v-if="!isOnlyOne">
+                <div
+                  v-for="(image, index) in  shopItem.images"
+                  :key="image.id"
+                  :class="['thumbnail-image image', (activeImage == index) ? 'active' : '']"
+                  v-on:click="activateImage(index)"
+                >
+                  <img :src="image.img">
+                </div>
+              </div>
+            </div>
+          </div>
 
       <div class="leftMargin column">
         <h1>{{ shopItem.name }}</h1>
@@ -56,21 +68,24 @@
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import { Component, Prop } from "vue-property-decorator";
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 
-  import axios, { AxiosResponse, AxiosError } from "axios";
-  import { APIConfig } from "@/utils/api.utils";
-  import { iShopItem, iImg } from "@/models/shopitem.interface";
-  import { } from "@/models/shopItem.interface";
+import axios, { AxiosResponse, AxiosError } from "axios";
+import { APIConfig } from "@/utils/api.utils";
+import { iShopItem, iImg } from "@/models/shopitem.interface";
+import {} from "@/models/shopItem.interface";
 
-  @Component
-  export default class ItemView extends Vue {
-    error: string | boolean = false;
-    loaded: boolean = false;
-    //someImages: iImg[] = [{id: 0, img: ""}];
-    shopItem: iShopItem | undefined;
-    //  { id: 0, name: "", price: 0, details: "", quantity: 0, category: "", inStorePickup: false, postedDate: new Date("2019-02-27"), images: this.someImages };
+@Component
+export default class ItemView extends Vue {
+  error: string | boolean = false;
+  loaded: boolean = false;
+  //someImages: iImg[] = [{id: 0, img: ""}];
+  shopItem: iShopItem | undefined;
+  //  { id: 0, name: "", price: 0, details: "", quantity: 0, category: "", inStorePickup: false, postedDate: new Date("2019-02-27"), images: this.someImages };
+
+  activeImage: number = 0;
+  currentImage: String = "";
 
   mounted() {
     this.display();
@@ -83,49 +98,139 @@
         this.shopItem = response.data;
         this.$emit("success");
         console.log(this.shopItem);
+        this.activateImage(0);
         this.loaded = true;
       })
       .catch((res: AxiosError) => {
-          this.error = res.response && res.response.data.error;
+        this.error = res.response && res.response.data.error;
       });
-    }
   }
+
+  activateImage(imageIndex) {
+    this.activeImage = imageIndex;
+    this.currentImage = this.shopItem.images[imageIndex].img
+      ? this.shopItem.images[imageIndex].img
+      : "";
+  }
+
+  get isOnlyOne(): boolean {
+    if( this.shopItem && (this.shopItem.images.length === 1))
+      return true;
+    else
+      return false;
+  }
+}
 </script>
 
 <style scoped lang="scss">
-  h1 {
-    margin-top: 90px;
-    font-size: 30px;
-    font-weight: bold;
-  }
-  hr {
-     background-color: black;
-     height: 2px;
-     border: 0;
-  }
-  h2 {
-    font-size: 22px;
-  }
-  .rightMargin {
-    margin-right:80px;
-  }
-  .leftMargin {
-    margin-left:80px;
-  }
-  .buttonStyle {
-    padding-top:5px;
-    padding-right:17px;
-    padding-bottom:5px;
-    padding-left:17px;
-    margin-bottom: 4px;
-  }
-  article {
-    height:300px;
-    width:400px;
-  }
-  .image {
-    width: 100%;
-    height: auto;
-    margin-top: 150px;
-  }
+.card-carousel {
+  user-select: none;
+  position: relative;
+}
+
+.thumbnails {
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+}
+
+.thumbnail-image {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 2px;
+}
+
+.thumbnail-image > img {
+  width: 100%;
+  height: auto;
+  transition: all 250ms;
+}
+
+.thumbnail-image > img {
+  width: 100%;
+  height: auto;
+  transition: all 250ms;
+}
+
+.thumbnail-image:hover > img,
+.thumbnail-image.active > img {
+  opacity: 0.6;
+  box-shadow: 2px 2px 6px 1px rgba(0, 0, 0, 0.5);
+}
+
+.card-img {
+  position: relative;
+  margin-bottom: 15px;
+}
+
+.card-img > img {
+  display: block;
+  margin: 0 auto;
+}
+
+.actions {
+  font-size: 1.5em;
+  height: 40px;
+  position: absolute;
+  top: 50%;
+  margin-top: -20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #585858;
+}
+
+.actions > span {
+  cursor: pointer;
+  transition: all 250ms;
+}
+
+.actions > span.prev {
+  margin-left: 5px;
+}
+
+.actions > span.next {
+  margin-right: 5px;
+}
+
+.actions > span:hover {
+  color: #eee;
+}
+h1 {
+  margin-top: 90px;
+  font-size: 30px;
+  font-weight: bold;
+}
+hr {
+  background-color: black;
+  height: 2px;
+  border: 0;
+}
+h2 {
+  font-size: 22px;
+}
+.rightMargin {
+  margin-right: 80px;
+}
+.leftMargin {
+  margin-left: 80px;
+}
+.buttonStyle {
+  padding-top: 5px;
+  padding-right: 17px;
+  padding-bottom: 5px;
+  padding-left: 17px;
+  margin-bottom: 4px;
+}
+article {
+  height: 300px;
+  width: 400px;
+}
+.image {
+  width: 100%;
+  height: auto;
+  margin-top: 150px;
+}
 </style>
